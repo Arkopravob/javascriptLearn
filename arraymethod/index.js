6b3e9b7b490aba6979d6d37a5f4346d1cd6c1540
Arkopravob <arka.bhuin@gmail.com>
2025-02-16 12:29:25 +0530

    add

---
+++ arraymethod/index.js
@@ -0,0 +1,30 @@
+const historMarks = [10, 20, 30, 40, 50];
+console.log(historMarks);
+const studentDetails = ['array', 73838, 'arko', 'data'];
+console.log(studentDetails);// 10
+const arr = new Array('simran', 'rahul', 'rohit', 'mohit');
+console.log(arr);
+//arays in js are dynamic in nature
+// we can add or remove elements from array
+const cites = ['delhi', 'mumbai', 'kolkata', 'chennai'];
+console.log(cites);
+
+// cites[2] = 'bangalore';
+// console.log(cites);
+
+/*method of array */
+//1. length
+console.log(cites.length)
+//2. push method
+const teams = ['india', 'australia', 'england', 'newzeland'];
+console.log(teams);
+teams.push('kkr');
+console.log(teams);
+
+
+//unshift method
+teams.unshift('rcb');
+console.log(teams);
+//removing an element from the end of an array
+teams.pop();
+console.log(teams);
