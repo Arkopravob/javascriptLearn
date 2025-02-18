9e198bc2fe820b3f36e7b512040b7233b95beaa4
Arkopravob <arka.bhuin@gmail.com>
2025-02-18 21:49:28 +0530

    string

---
+++ String/string.js
@@ -0,0 +1,14 @@
+/**String Traversal */
+
+let str = "Hello World";
+for (let i = 0; i < str.length; i++) {
+    console.log(str.charAt(i));
+}
+
+//inludes()
+console.log(str.includes("is"));
+//tirm()
+console.log(str.trim());
+//split()
+let word = str.split(',');
+console.log(word);
