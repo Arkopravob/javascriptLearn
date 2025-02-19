3ec91323dad89c44c39be8367dcc93f4672ce10a
Arkopravob <arka.bhuin@gmail.com>
2025-02-19 16:04:53 +0530

    appjs

---
+++ todoapp/app.js
@@ -0,0 +1,16 @@
+let todo = [];
+let req = prompt("please enter your task");
+
+while (true) {
+    if (req == "quit") {
+        console.log("quitting  app");
+        break;
+    }
+}
+if (req == "list") {
+    for (task of todo) {
+        console.log(task);
+    }
+} else if (req == "add") {
+    let task = prompt("please eneter ")
+}
