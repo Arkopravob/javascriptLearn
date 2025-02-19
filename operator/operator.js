5157638ef74618a7f506c78ac2a2bab8f471197c
Arkopravob <arka.bhuin@gmail.com>
2025-02-19 08:36:16 +0530

    operator

---
+++ operator/operator.js
@@ -0,0 +1,22 @@
+//operator js
+//arithmatic operator
+let num1 = 45;
+let num2 = 40;
+
+console.log(`addition: ${num1 + num2}`);
+console.log(`multiplication :  ${num1 * num2}`);
+console.log(`division : ${num1 / num2}`);
+//remainder
+console.log(`remainder :  ${num1 % 10}`);
+
+//incrementing ops
+num1++;
+let z = num1;
+console.log(`increment ops : ${z}`);
+// decrement ops
+num2--;
+let y = num2;
+console.log(`decrement ops:  ${y}`);
+
+//exponent ops
+console.log(`exponent : ${num2 ** 2}`);
